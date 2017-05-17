a3b38e7a2c9eb9667207e6eaba482d692a52c3de
jsivakumaran <jsivakumaran@outlook.com>
2017-05-16 20:50:24 -0400

    Add ability to server images,html, css, and js files as well as a couple of test files to test whether the server's basic functionality works

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
 const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = '1337';
+const mimeTypes = {
+  "html": "text/html",
+  "jpeg": "image/jpeg",
+  "jpg": "image/jpg",
+  "png": "image/png",
+  "js": "text/javascript",
+  "css": "text/css"
+};
 
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+http.createServer(function(req, res){
+    var uri = url.parse(req.url).pathname;
+    var fileName = path.join(process.cwd(), unescape(uri));
+    console.log('Loading' + uri);
+    var stats;
 
-}).listen(port, hostname, ()=> {
-  console.log(`Server running at http://${hostname}:${port}`);
-});
+    try{
+        stats = fs.lstatSync(fileName);
+    } catch(e){
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('404 not found\n');
+        res.end();
+        return;
+    }
+
+    if(stats.isFile()){
+      var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+      res.writeHead(200, {'Content-Type': mimeType});
+
+      var fileStream = fs.createReadStream(fileName);
+      fileStream.pipe(res);
+    }else if(stats.isDirectory){
+      res.writeHead(302, {
+        'Location': 'index.html'
+      });
+      res.end();
+    }else{
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.write('500 Internal Error\n');
+      res.end();
+    }
+}).listen(1337);
